290cb44f262858d6ab6174e0f2d5326fe4d0fd35
vargapatrikadam <patrikadamvarga@gmail.com>
2019-11-16 21:39:27 +0100

    refactored controller module namings

--- controller/src/main/java/gamestore/controllers/GameStoreController.java
+++ controller/src/main/java/gamestore/controllers/GameStoreController.java
@@ -1,4 +1,4 @@
-package controllers;
+package gamestore.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;

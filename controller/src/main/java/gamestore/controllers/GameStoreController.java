ad53b721e16df01de368cfcd848e8425cf4331b4
vargapatrikadam <patrikadamvarga@gmail.com>
2019-11-17 01:04:59 +0100

    added get games & create new game methods to controller

--- controller/src/main/java/gamestore/controllers/GameStoreController.java
+++ controller/src/main/java/gamestore/controllers/GameStoreController.java
@@ -1,11 +1,19 @@
 package gamestore.controllers;
 
+import gamestore.exceptions.DateIsTooLate;
+import gamestore.exceptions.NoMatchingId;
+import gamestore.models.Category;
+import gamestore.models.Game;
+import gamestore.models.Platform;
+import gamestore.models.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import services.GameStoreService;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
 public class GameStoreController {
@@ -19,5 +27,18 @@ public class GameStoreController {
     @ResponseBody
     public String showGamesCount(){
         return String.valueOf(service.getAllGames().size());
+    }
+
+    @RequestMapping(value = "/getgames", method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<Game> getAllGames() throws DateIsTooLate {
+        return service.getAllGames();
+    }
+
+    @RequestMapping(value = "/addnewgame", method = RequestMethod.POST)
+    @ResponseBody
+    public Game addNewGame(@RequestBody Game game) throws DateIsTooLate, NoMatchingId {
+        service.addGame(game);
+        return service.getGame(game.getId());
     }
 }

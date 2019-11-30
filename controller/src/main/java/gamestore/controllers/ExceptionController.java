977c99eb9fc0fbd4a885f8f701f0010121028581
vargapatrikadam <patrikadamvarga@gmail.com>
2019-11-30 14:54:16 +0100

    added central exception handler with handle invalid format exception

--- controller/src/main/java/gamestore/controllers/ExceptionController.java
+++ controller/src/main/java/gamestore/controllers/ExceptionController.java
@@ -1,4 +1,19 @@
 package gamestore.controllers;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String noEnumValueException(Exception ex){
+        return "Invalid enum value: " + ex.getMessage();
+    }
 }
